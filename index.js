e077f0dad0eb992cbf8f0fed52859f5b34616094
seun <oluwaseunoyebowale@yahoo.com>
2023-03-20 02:30:46 +0100

    final commit

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const path = require("path");
 const app = new express();
+const mailgun = require("mailgun-js")
+const api_key = "7cd6e59c0c7b00edb9fa3fd601be7981-30344472-525df09d"
+const domain = "sandboxbf551d2c3ba94ffcaa80235c39bbe01c.mailgun.org"
+const mg = mailgun({apiKey: api_key, domain: domain});
+
+
+// const mg = mailgun.client({
+// 	username: process.env.USER,
+// 	key: process.env.PASS,
+
+// });
 
 const port = 3000;
+
+app.use(express.json())
+
 
 app.use(express.static(path.join(__dirname, './static')));
 
@@ -12,26 +26,43 @@ app.get('/', (request, response) => {
 })
 
 app.post('/contact', (request, response) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com", //replace with your email provider
-    port: 587,
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASS,
-    },
-  });
-
+  // const transporter = nodemailer.createTransport({
+  //   host: "smtp.sendgrid.net", //replace with your email provider
+  //   port: 25,
+  //   auth: {
+  //     user: process.env.EMAIL,
+  //     pass: process.env.PASS,
+  //   },
+  // });
   var textBody = `FROM: ${request.body.name} EMAIL: ${request.body.email} MESSAGE: ${request.body.message}`;
   var htmlBody = `<h2>Mail From Contact Form</h2><p>from: ${request.body.name} <a href="mailto:${request.body.email}">${request.body.email}</a></p><p>${request.body.message}</p>`;
   var mail = {
     from: "your_account@gmail.com", // sender address
-    to: "your_account@gmail.com", // list of receivers (THIS COULD BE A DIFFERENT ADDRESS or ADDRESSES SEPARATED BY COMMAS)
+    to: "oyebowalemuiz@gmail.com", // list of receivers (THIS COULD BE A DIFFERENT ADDRESS or ADDRESSES SEPARATED BY COMMAS)
     subject: "Mail From Contact Form", // Subject line
     text: textBody,
     html: htmlBody
   };
 
-  transporter.sendMail(mail, function (err, info) {
+  // mg.messages
+	// .create(sandboxbf551d2c3ba94ffcaa80235c39bbe01c.mailgun.org, {
+	// 	from: "Mailgun Sandbox <postmaster@sandboxbf551d2c3ba94ffcaa80235c39bbe01c.mailgun.org>",
+	// 	to: ["oyebowalemuiz@gmail.com"],
+	// 	subject: "Hello",
+	// 	text: textBody,
+  //   html: htmlBody
+	// })
+	// .then(msg => {
+    
+  //   console.log(msg)
+  //   response.json({ message: `message sent` });
+  // }) // logs response data
+	// .catch(err => {
+  //   console.log(err)
+  //   response.json({ message: "message not sent: an error occured; check the server's console log" });
+  // }); 
+
+  mg.messages().send(mail, function (err, info) {
 		if(err) {
 			console.log(err);
 			response.json({ message: "message not sent: an error occured; check the server's console log" });
